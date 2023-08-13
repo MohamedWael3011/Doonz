c7c77bf1ba60c21aafd0dacbcff1657ee1a70bba
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2023-08-14 00:33:43 +0300

    Adding Cogs

---
+++ cogs/cog1.py
@@ -0,0 +1,225 @@
+from typing import Optional
+import discord
+from discord.ext import commands,tasks
+from discord import app_commands
+import os
+from dotenv import load_dotenv
+import asyncio
+from discord.ui import Button
+import datetime
+from discord.app_commands import Choice
+import json
+import aiohttp
+import re
+import random
+ShopItems = {
+    "DOONZ NFT AIRDROP 🎈" :(30000,"Your ticket to claim a NFT from Doonz NFT Editions Collections on OpenSea"),
+    "DOONZ EDITIONS NFT 🎨 " :(20000,"Your ticket to clain a Doonz NFT airdrop."),
+    "RAFFLE TICKET 🎟️" :(1000,"Your ticked to join special raffles here on discord. **Please not that you will be granted the role after claiming this shop item and role will be removed after the raffle ends.**"), 
+}
+
+
+
+
+
+    
+async def getBankData():
+    with open ("bank.json","r") as f:
+        users = json.load(f)
+    return users    
+    
+async def removeMoney(userid,value):
+    users = await getBankData()
+    users[userid]["balance"] -= value  
+    with open ("bank.json","w") as f:
+        json.dump(users,f)
+        
+async def addMoney(userid,value):
+    users = await getBankData()
+    users[userid]["balance"] += value  
+    with open ("bank.json","w") as f:
+        json.dump(users,f)
+async def isUser(userid):
+    users = await getBankData()
+    if userid in users:
+        return True
+    return False
+
+async def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+
+    return hours, minutes, seconds
+
+async def holder(interaction: discord.Interaction):
+  role = interaction.guild.get_role(1123573342738329650)
+  if role in interaction.user.roles:
+    return True
+  await interaction.response.send_message(
+    "<@{}> I am sorry only Doonz Holders can use me.".format(
+      interaction.user.id),
+    ephemeral=True)
+  return False
+
+async def owner(interaction: discord.Interaction):  # Me or Nash
+    ownerRole = interaction.guild.get_role(1136244685116014684)
+    modRole = interaction.guild.get_role(1138232772452941984) 
+    if (ownerRole in interaction.user.roles) or (modRole in interaction.user.roles):
+        return True
+    await interaction.response.send_message(
+    "<@{}> I am sorry only Admins can use me.".format(
+      interaction.user.id),
+    ephemeral=True)
+    return False    
+    
+class SimpleView(discord.ui.View):
+    
+        
+    def __init__(self, timeout=None):
+        super().__init__(timeout=timeout)
+        self.role = None
+        self.ListofPeople = []
+        self.em = None
+        self.message = None
+        
+        
+    @discord.ui.button(label="Enter Giveaway")
+    async def GiveAway(self,interaction: discord.Interaction,button:discord.ui.Button):
+        if not (self.role in interaction.user.roles):
+            await interaction.response.send_message(f"I am sorry, only people with <@&{self.role.id}> can join.",ephemeral=True)
+            
+        if interaction.user in self.ListofPeople:
+            await interaction.response.send_message("You have already joined this giveaway!",ephemeral=True)
+        else:
+            await interaction.response.send_message("You have joined this giveaway!",ephemeral=True)
+            self.ListofPeople.append(interaction.user)
+            self.em.set_field_at(4,name="",value= f":large_blue_diamond: Number of Participants: {str(len(self.ListofPeople))}",inline= False)
+            await self.message.edit_original_response(embed=self.em,view=self)
+            
+
+async def CreateAccount(userid,matic,twitter):
+    users = await getBankData()
+    users[userid] = {"balance": 0, "WalletAddress": matic,"TwitterAccount":twitter}
+    with open ("bank.json","w") as f:
+        json.dump(users,f)    
+          
+          
+          
+          
+class StartCog(commands.Cog):
+    def __init__(self,client):
+        self.client = client
+            
+            
+        
+    @app_commands.command(name="user_info", description="Displays user's info")
+    async def user_info(self,interaction: discord.Interaction,user: discord.User): 
+        validuser = await isUser(str(user.id))
+        if validuser:
+            users = await getBankData()
+            em = discord.Embed(title=f"{user.name}'s Info",color= discord.Color.yellow())
+            Balance = users[str(user.id)]["balance"]
+            Address = users[str(user.id)]["WalletAddress"]
+            Twiiter = users[str(user.id)]["TwitterAccount"]
+            em.add_field(name="",value=f"User's Balance: {Balance}",inline=False)
+            em.add_field(name="",value=f"User's ETH Address: {Address}",inline=False)
+            em.add_field(name="",value=f"User's Twiiter Account: {Twiiter}",inline=False)
+            await interaction.response.send_message(embed=em)
+        else:
+            await interaction.response.send_message("This user isn't registered in our system",ephemeral=True)
+        
+        
+                
+    @app_commands.command(name="edit_matic_wallet", description="Changes the matic wallet you saved in the eco bot")
+    async def edit_matic_wallet(self,interaction: discord.Interaction,matic_address: str): 
+        validUser = await isUser(str(interaction.user.id))
+        if validUser:
+            users = await getBankData()
+            users[str(interaction.user.id)]["WalletAddress"] = matic_address
+            with open ("bank.json","w") as f:
+                json.dump(users,f)
+            await interaction.response.send_message(f"Your wallet address has been updated to `{matic_address}`.",ephemeral=True)
+        else:
+            await interaction.response.send_message(content="Please create an account first by using `/joindoonz` command.",ephemeral=True)
+            
+            
+    @app_commands.command(name="edit_twiiter_account", description="Changes the twitter account you saved in the eco bot")
+    async def edit_twiiter_account(self,interaction: discord.Interaction,twitter_acount: str): 
+        validUser = await isUser(str(interaction.user.id))
+        if validUser:
+            users = await getBankData()
+            users[str(interaction.user.id)]["TwitterAccount"] = twitter_acount
+            with open ("bank.json","w") as f:
+                json.dump(users,f)
+            await interaction.response.send_message(f"Your wallet address has been updated to `{twitter_acount}`.",ephemeral=True)
+        else:
+            await interaction.response.send_message(content="Please create an account first by using `/joindoonz` command.",ephemeral=True)
+            
+            
+    @app_commands.command(name="shop", description="Preview all items in shop")
+    async def shop(self,interaction: discord.Interaction):
+        em = discord.Embed(title="🛒COINZ SHOP🛒",color=discord.Color.yellow())
+        for key,value in ShopItems.items():
+            em.add_field(name= f"{key} - {value[0]} Doonz Coinz :moneybag:",value=value[1],inline=False) 
+        await interaction.response.send_message(embed=em,ephemeral=True)
+            
+
+    def loadShopChoices():
+        ls = []
+        for key,value in ShopItems.items():
+            ls.append(Choice(name=key, value=value[0]))
+        return ls
+            
+
+        
+    @app_commands.command(name="buy_item", description="To purchase items from the shop")
+    @app_commands.choices(
+        shop_item =  loadShopChoices()
+    )
+    async def buy_item(self,interaction: discord.Interaction,shop_item: Choice[int]):
+        # Reading user balance
+        users = await getBankData()
+        UserBalance = users[str(interaction.user.id)]["balance"]
+        if UserBalance < shop_item.value:
+            await interaction.response.send_message("You don't have enough coinz!",ephemeral=True)
+        else:
+            if(shop_item.name == "Doonz Raffle Ticket"): # Add role logic
+                RaffleRole = discord.utils.get(interaction.guild.roles, name="Raffle Ticket")
+                if RaffleRole in interaction.user.roles:
+                    await interaction.response.send_message("You already have this role!",ephemeral=True)
+                    return
+                await interaction.user.add_roles(RaffleRole)
+                await interaction.response.send_message(f"**Raffle Ticket** role has been added. Good luck in the raffle!")
+            else:
+                await interaction.response.send_message(f"{shop_item.name} has been purchased, please open a <#1124318043355480115> to claim it!")   
+            await removeMoney(str(interaction.user.id),shop_item.value)
+            
+                
+                
+                
+    @app_commands.command(name="leaderboard", description="Displays the leaderboard!")
+    async def leaderboard(self,interaction: discord.Interaction):
+        users = await getBankData()
+        em = discord.Embed(title="Doonz Coin Leaderboard",color=discord.Color.yellow())
+        Leaderboard = []
+        for user in users:
+            Leaderboard.append((users[user]["balance"],user))
+        Leaderboard.sort(key=lambda tup: tup[0],reverse=True)
+        for i, entry in enumerate(Leaderboard[:10], start=1):
+            em.add_field(name="", value=f"{i}- <@{entry[1]}> ({entry[0]})",inline=False)
+        await interaction.response.send_message(embed=em)
+
+    @app_commands.command(name="add_money", description="adds money to a user")
+    @commands.check(owner)
+    async def add_money(self,interaction: discord.Interaction,send_to:discord.User,amount: str):
+        await addMoney(str(send_to.id),int(amount))
+        em = discord.Embed(title="Doonz Coin Leaderboard",color=discord.Color.yellow())
+        em.add_field(name="",value=f"added {amount} :moneybag: to <@{send_to.id}>")
+        await interaction.response.send_message(embed=em)
+    
+
+
+
+async def setup(client:commands.Bot) -> None:
+    await client.add_cog(StartCog(client))
--- main.py
+++ main.py
@@ -12,16 +12,22 @@ import json
 import aiohttp
 import re
 import random
+from cogs.cog1 import *
 load_dotenv()
 intents = discord.Intents.all()
 
-client = commands.Bot(command_prefix='.', intents=intents)
+class Client(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned_or('.'), intents=intents)
+    
+    async def setup_hook(self):
+        await self.load_extension("cogs.cog1")
+    
+    async def on_ready(self):
+        print('Doonz is running! Currently serving {0.user}'.format(client))
+        await self.tree.sync()
+    
 
-ShopItems = {
-    "Doonz Editions NFT - Claimable Ticket -" :(4000,"Your ticket to claim a NFT from Doonz NFT Editions Collections on OpenSea"),
-    "Doonz NFT Airdrop - Claimable Ticket -" :(6000,"Your ticket to clain a Doonz NFT airdrop."),
-    "Doonz Raffle Ticket" :(300,"Your ticked to join special raffles here on discord. **Please not that you will be granted the role after claiming this shop item and role will be removed after the raffle ends.**"), 
-}
 
 RoleMappedToReward = {
     1136242811004518490: 500, 
@@ -32,48 +38,6 @@ RoleMappedToReward = {
     1123573342738329650:10, 
 }
 
-
-
-    
-async def getBankData():
-    with open ("bank.json","r") as f:
-        users = json.load(f)
-    return users    
-    
-async def removeMoney(userid,value):
-    users = await getBankData()
-    users[userid]["balance"] -= value  
-    with open ("bank.json","w") as f:
-        json.dump(users,f)
-        
-async def addMoney(userid,value):
-    users = await getBankData()
-    users[userid]["balance"] += value  
-    with open ("bank.json","w") as f:
-        json.dump(users,f)
-async def isUser(userid):
-    users = await getBankData()
-    if userid in users:
-        return True
-    return False
-
-async def convert_seconds(seconds):
-    hours = seconds // 3600
-    minutes = (seconds % 3600) // 60
-    seconds = seconds % 60
-
-    return hours, minutes, seconds
-
-async def holder(interaction: discord.Interaction):
-  role = interaction.guild.get_role(1123573342738329650)
-  if role in interaction.user.roles:
-    return True
-  await interaction.response.send_message(
-    "<@{}> I am sorry only Doonz Holders can use me.".format(
-      interaction.user.id),
-    ephemeral=True)
-  return False
-
 async def owner(interaction: discord.Interaction):  # Me or Nash
     ownerRole = interaction.guild.get_role(1136244685116014684)
     modRole = interaction.guild.get_role(1138232772452941984) 
@@ -83,42 +47,19 @@ async def owner(interaction: discord.Interaction):  # Me or Nash
     "<@{}> I am sorry only Admins can use me.".format(
       interaction.user.id),
     ephemeral=True)
-    return False    
-    
-class SimpleView(discord.ui.View):
-    
-        
-    def __init__(self, timeout=None):
-        super().__init__(timeout=timeout)
-        self.role = None
-        self.ListofPeople = []
-        self.em = None
-        self.message = None
-        
-        
-    @discord.ui.button(label="Enter Giveaway")
-    async def GiveAway(self,interaction: discord.Interaction,button:discord.ui.Button):
-        if not (self.role in interaction.user.roles):
-            await interaction.response.send_message(f"I am sorry, only people with <@&{self.role.id}> can join.",ephemeral=True)
-            
-        if interaction.user in self.ListofPeople:
-            await interaction.response.send_message("You have already joined this giveaway!",ephemeral=True)
-        else:
-            await interaction.response.send_message("You have joined this giveaway!",ephemeral=True)
-            self.ListofPeople.append(interaction.user)
-            self.em.set_field_at(4,name="",value= f":large_blue_diamond: Number of Participants: {str(len(self.ListofPeople))}",inline= False)
-            await self.message.edit_original_response(embed=self.em,view=self)
-            
+    return False     
 
-  
+async def holder(interaction: discord.Interaction):
+  role = interaction.guild.get_role(1123573342738329650)
+  if role in interaction.user.roles:
+    return True
+  await interaction.response.send_message(
+    "<@{}> I am sorry only Doonz Holders can use me.".format(
+      interaction.user.id),
+    ephemeral=True)
+  return False     
 
-        
-@client.event
-async def on_ready():
-    print('Doonz is running! Currently serving {0.user}'.format(client))
-    await client.tree.sync()
-    #await dailygiveaway.start()
-
+client = Client()
 cd_mapping = commands.CooldownMapping.from_cooldown(1, 120, commands.BucketType.user)
 @client.event
 async def on_message(message):
@@ -134,11 +75,7 @@ async def on_message(message):
         await addMoney(str(message.author.id),random.randint(1, 5))
 
 
-async def CreateAccount(userid,matic,twitter):
-    users = await getBankData()
-    users[userid] = {"balance": 0, "WalletAddress": matic,"TwitterAccount":twitter}
-    with open ("bank.json","w") as f:
-        json.dump(users,f)    
+    
     
 
 
@@ -214,153 +151,11 @@ async def create_giveaway(interaction: discord.Interaction,giveaway_minutes: int
     await generategiveaway(interaction,giveaway_minutes,prize,winners,role_limit)
         
         
-@tasks.loop(hours=24)
-async def dailygiveaway():
-    guild = discord.utils.get(client.guilds, id=1123569723314020452)
-    channel = guild.get_channel(1139892842324566157)
-    #role = discord.utils.get(guild.get_role, id=1083474077282468021)
-    em = discord.Embed(title="Daily Coins Giveaway",color=discord.Color.yellow())
-    endTime = datetime.datetime.now() + datetime.timedelta(hours=24)
-    em.add_field(name="",value="Prize: 100 Coins",inline=False)
-    DisplayedTime = endTime.timestamp()
-    em.add_field(name="",value= f"Ends At: {discord.utils.format_dt(datetime.datetime.fromtimestamp(DisplayedTime), 'R')}",inline=False)
-    my_msg = await channel.send(embed=em)
-    await my_msg.add_reaction("🎉")
-    await asyncio.sleep(24*60*60)
-    new_msg = await channel.fetch_message(my_msg.id)
-    users = []
-    for reaction in new_msg.reactions:
-        if reaction.emoji == '🎉':
-            async for user in reaction.users():
-                if user != client.user:
-                    users.append(user)
-    try:
-        winner = random.choice(users)
-        await channel.send(f"Congratulations {winner.mention} for winning the daily coin giveaway!")
-        try:
-            await addMoney(str(winner.id),100)
-        except:
-            users = await getBankData()
-            users[str(winner.id)] = {"balance": 100, "WalletAddress": "","TwitterAccount":""}
-            with open ("bank.json","w") as f:
-                json.dump(users,f)
-    except:
-        pass
-        
-        
-    
-@client.tree.command(name="user_info", description="Displays user's info")
-async def user_info(interaction: discord.Interaction,user: discord.User): 
-    validuser = await isUser(str(user.id))
-    if validuser:
-        users = await getBankData()
-        em = discord.Embed(title=f"{user.name}'s Info",color= discord.Color.yellow())
-        Balance = users[str(user.id)]["balance"]
-        Address = users[str(user.id)]["WalletAddress"]
-        Twiiter = users[str(user.id)]["TwitterAccount"]
-        em.add_field(name="",value=f"User's Balance: {Balance}",inline=False)
-        em.add_field(name="",value=f"User's ETH Address: {Address}",inline=False)
-        em.add_field(name="",value=f"User's Twiiter Account: {Twiiter}",inline=False)
-        await interaction.response.send_message(embed=em)
-    else:
-        await interaction.response.send_message("This user isn't registered in our system",ephemeral=True)
-    
-    
-            
-@client.tree.command(name="edit_matic_wallet", description="Changes the matic wallet you saved in the eco bot")
-async def edit_matic_wallet(interaction: discord.Interaction,matic_address: str): 
-    validUser = await isUser(str(interaction.user.id))
-    if validUser:
-        users = await getBankData()
-        users[str(interaction.user.id)]["WalletAddress"] = matic_address
-        with open ("bank.json","w") as f:
-            json.dump(users,f)
-        await interaction.response.send_message(f"Your wallet address has been updated to `{matic_address}`.",ephemeral=True)
-    else:
-        await interaction.response.send_message(content="Please create an account first by using `/joindoonz` command.",ephemeral=True)
-        
-        
-@client.tree.command(name="edit_twiiter_account", description="Changes the twitter account you saved in the eco bot")
-async def edit_twiiter_account(interaction: discord.Interaction,twitter_acount: str): 
-    validUser = await isUser(str(interaction.user.id))
-    if validUser:
-        users = await getBankData()
-        users[str(interaction.user.id)]["TwitterAccount"] = twitter_acount
-        with open ("bank.json","w") as f:
-            json.dump(users,f)
-        await interaction.response.send_message(f"Your wallet address has been updated to `{twitter_acount}`.",ephemeral=True)
-    else:
-        await interaction.response.send_message(content="Please create an account first by using `/joindoonz` command.",ephemeral=True)
-        
-        
-@client.tree.command(name="shop", description="Preview all items in shop")
-async def shop(interaction: discord.Interaction):
-    em = discord.Embed(title="Doonz Shop",color=discord.Color.yellow())
-    for key,value in ShopItems.items():
-        em.add_field(name= f"{key} - {value[0]} :moneybag:",value=value[1],inline=False) 
-    await interaction.response.send_message(embed=em,ephemeral=True)
-        
 
-def loadShopChoices():
-    ls = []
-    for key,value in ShopItems.items():
-        ls.append(Choice(name=key, value=value[0]))
-    return ls
-        
 
-    
-@client.tree.command(name="buy_item", description="To purchase items from the shop")
-@app_commands.choices(
-    shop_item =  loadShopChoices()
-)
-async def buy_item(interaction: discord.Interaction,shop_item: Choice[int]):
-    # Reading user balance
-    users = await getBankData()
-    UserBalance = users[str(interaction.user.id)]["balance"]
-    if UserBalance < shop_item.value:
-        await interaction.response.send_message("You don't have enough coins!",ephemeral=True)
-    else:
-        if(shop_item.name == "Doonz Raffle Ticket"): # Add role logic
-            RaffleRole = discord.utils.get(interaction.guild.roles, name="Raffle Ticket")
-            if RaffleRole in interaction.user.roles:
-                await interaction.response.send_message("You already have this role!",ephemeral=True)
-                return
-            await interaction.user.add_roles(RaffleRole)
-            await interaction.response.send_message(f"**Raffle Ticket** role has been added. Good luck in the raffle!")
-        else:
-            await interaction.response.send_message(f"{shop_item.name} has been purchased, please open a <#1124318043355480115> to claim it!")   
-        await removeMoney(str(interaction.user.id),shop_item.value)
-        
-            
-            
-              
-@client.tree.command(name="leaderboard", description="Displays the leaderboard!")
-async def leaderboard(interaction: discord.Interaction):
-    users = await getBankData()
-    em = discord.Embed(title="Doonz Coin Leaderboard",color=discord.Color.yellow())
-    Leaderboard = []
-    for user in users:
-        Leaderboard.append((users[user]["balance"],user))
-    Leaderboard.sort(key=lambda tup: tup[0])
-    for i in range(10):
-        if i < len(Leaderboard):
-            em.add_field(name="",value=f"{i+1}- <@{Leaderboard[i][1]}> ({Leaderboard[i][0]})")
-    await interaction.response.send_message(embed=em)
-
-@client.tree.command(name="add_money", description="adds money to a user")
-@commands.check(owner)
-async def add_money(interaction: discord.Interaction,send_to:discord.User,amount: str):
-    await addMoney(str(send_to.id),int(amount))
-    em = discord.Embed(title="Doonz Coin Leaderboard",color=discord.Color.yellow())
-    em.add_field(name="",value=f"added {amount} :moneybag: to <@{send_to.id}>")
-    await interaction.response.send_message(embed=em)
- 
 HelperDec = {0:"Head",
-             1:"Tail"}
-
-
-
-@client.tree.command(name="headortail", description="If you win it gives you double the amount you bet, if you lose you lose your bet :( MAXIMUM BET: 100")
+                1:"Tail"}
+@client.tree.command(name="headsortails", description="If you win it gives you double the amount you bet, if you lose you lose your bet :( MAXIMUM BET: 100")
 @app_commands.choices(
     choice =  [Choice(name="Head", value=0),Choice(name="Tail",value=0)]
 )
@@ -381,14 +176,14 @@ async def headortail(interaction: discord.Interaction,bet: app_commands.Range[in
     
     if validUser:
         botChoice = random.randint(0, 1)
-        em = discord.Embed(title="Head or Tail",color=discord.Color.yellow())
+        em = discord.Embed(title="Heads or Tails",color=discord.Color.yellow())
         em.add_field(name=f":coin: : {HelperDec[botChoice]}",value="",inline=False)
         
         if botChoice == choice.value:
-            em.add_field(name="",value=f"You guessed correct! You won {bet*2} coins",inline=False)
+            em.add_field(name="",value=f"You guessed correct! You won {bet*2} coinz",inline=False)
             await addMoney(str(interaction.user.id),bet)
         else:
-            em.add_field(name="",value=f"You didn't guess correct :( You lost {bet} coins",inline=False)
+            em.add_field(name="",value=f"You didn't guess correct :( You lost {bet} coinz",inline=False)
             await removeMoney(str(interaction.user.id),bet)
         await interaction.response.send_message(embed=em)
     else:
@@ -404,12 +199,17 @@ async def on_test_error(interaction: discord.Interaction,error: app_commands.App
             
 
         
-  
+@client.tree.command(name="reload", description="Reloads the bot")
+@commands.check(owner)
+async def reload(interaction: discord.Interaction): 
+    await client.reload_extension(f"cogs.cog1")
+    embed = discord.Embed(title='Reload', description='successfully reloaded', color=0xff00c8)
+    await interaction.response.send_message(embed=embed)
                 
-@client.tree.command(name="dailycoins", description="Collect daily coins")
+@client.tree.command(name="dailycoinz", description="Collect daily coinz")
 @commands.check(holder)
 @app_commands.checks.cooldown(1, 24*60*60, key=lambda i: (i.user.id))
-async def dailycoins(interaction: discord.Interaction):
+async def dailycoinz(interaction: discord.Interaction):
     users = await getBankData()
     validUser = await isUser(str(interaction.user.id))
 
@@ -429,7 +229,7 @@ async def dailycoins(interaction: discord.Interaction):
             return
     
         
-@dailycoins.error
+@dailycoinz.error
 async def on_test_error(interaction: discord.Interaction,error: app_commands.AppCommandError):
    if isinstance(error, app_commands.CommandOnCooldown):
      RemainingHours,RemainingMin,RemainingSec = await convert_seconds(error.retry_after)
